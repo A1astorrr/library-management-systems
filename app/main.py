3e04864b11fbb79f223ddbc6e1e236f60f1ed883
A1astorrr <meshcheryakov.v@list.ru>
2024-11-22 13:20:44 +0300

    creating main

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+@app.get("/",
+         tags=["Приветствие"])
+def welcome():
+    return {"message": "Welcome"}
+
+
+if __name__  == "__main__":
+    uvicorn.run("app.main:app", reload=True)
