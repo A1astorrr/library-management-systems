ab235e3d770f2e9555bda22140623d1e4be89926
A1astorrr <meshcheryakov.v@list.ru>
2024-11-22 13:22:59 +0300

    creating config, db

---
+++ app/database.py
@@ -0,0 +1,17 @@
+from  pydantic_settings  import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "name"
+    DB_USER: str = "username"
+    DB_PASS: str = "password"
+    
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
